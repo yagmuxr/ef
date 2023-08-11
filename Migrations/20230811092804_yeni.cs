b08b68d43d673d1b090cc563a5cd2289f75a418e
Yağmur Yalçın <yagmuryalcin26@hotmail.com>
2023-08-11 15:11:49 +0300

    first commit

---
+++ Migrations/20230811092804_yeni.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace yeniyeni.Migrations
+{
+    /// <inheritdoc />
+    public partial class yeni : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    ProductId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", maxLength: 100, nullable: false),
+                    Price = table.Column<decimal>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.ProductId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Categories");
+
+            migrationBuilder.DropTable(
+                name: "Products");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+
+namespace yeniyeni
+{
+internal class Program
+{
+    public class ShopContext : DbContext
+    {
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories {get;set;}
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            
+            optionsBuilder.UseSqlite("Data Source=shop.db");
+        }
+    }
+
+    public class Product
+    {
+    public int ProductId { get; set; }
+    
+    [MaxLength(100)]
+    [Required]
+    public string Name { get; set; }
+    
+    public decimal Price { get; set; }
+    }
+    public class Category
+    {
+        public int Id { get; set;}
+        public string Name { get; set; }
+    }
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World!");
+    }
+}
+}
